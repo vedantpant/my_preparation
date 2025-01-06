651fd4866b2dcf47b8d0f61e1fc265026bf94490
Vedant Pant <vedantant@gmail.com>
2025-01-07 03:41:50 +0530

    changes update for day1...tobecontinued

--- Day1/Day1/main.py
+++ Day1/Day1/main.py
@@ -30,3 +30,84 @@ print({'name':'Nitish','gender':'Male','weight':70})
 
 # type
 type([1,2,3])
+
+a = 5
+print(type(a))
+
+b = str(a)
+print(type(b))
+
+a = 0b1010 #Binary Literals
+b = 100 #Decimal Literal
+c = 0o310 #Octal Literal
+d = 0x12c #Hexadecimal Literal
+
+#Float Literal
+float_1 = 10.5
+float_2 = 1.5e2 # 1.5 * 10^2
+float_3 = 1.5e-3 # 1.5 * 10^-3
+
+#Complex Literal
+x = 3.14j
+
+print(a, b, c, d)
+print(float_1, float_2,float_3)
+print(x, x.imag, x.real)
+
+string = 'This is Python'
+strings = "This is Python"
+char = "C"
+multiline_str = """This is a multiline string with more than one line code."""
+unicode = u"\U0001f600\U0001F606\U0001F923"
+raw_str = r"raw \n string"
+
+print(string)
+print(strings)
+print(char)
+print(multiline_str)
+print(unicode)
+print(raw_str)
+
+# Arithmetric Operators
+print(5 - 6)  # Corrected expression
+print(5 * 6)  # Corrected expression
+print(5 / 2)  # Corrected expression
+print(5 // 2)  # Corrected expression
+print(5 % 2)  # Corrected expression
+print(5 ** 2)  # Corrected expression
+
+# Relational Operators
+print(4 > 5)  # Corrected expression
+print(4 < 5)  # Corrected expression
+print(4 >= 4)  # Corrected expression
+print(4 <= 4)  # Corrected expression
+print(4 == 4)  # Corrected expression
+print(4 != 4)  # Corrected expression
+
+# Logical Operators
+print(1 and 0)  # Corrected expression
+print(1 or 0)  # Corrected expression
+print(not 1)  # Corrected expression
+
+# Bitwise Operators
+
+# bitwise and
+print(2 & 3)  # Corrected expression
+# bitwise or
+print(2 | 3)  # Corrected expression
+
+# bitwise xor
+print(2 ^ 3)  # Corrected expression
+print(~3)  # Corrected expression
+print(4 >> 2)  # Corrected expression
+print(5 << 2)  # Corrected expression
+
+# Assignment Operators
+a = 2
+a %= 2
+print(a)  # Corrected expression
+
+# Membership Operators
+# in/not in
+print('D' not in 'Delhi')  # Corrected expression
+print(1 in [2, 3, 4, 5, 6])  # Corrected expression
