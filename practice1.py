a96ac9aa0b93b8da233a46906deae31682d1b70e
Vedant Pant <vedantant@gmail.com>
2025-01-08 02:51:58 +0530

    classes and objects day 2...

--- practice1.py
+++ practice1.py
@@ -228,26 +228,30 @@
 # else:
 #     print("no")
 
-arr = [0, 1, 0, 1, 0, 0, 1, 1, 1, 0]
+# arr = [0, 1, 0, 1, 0, 0, 1, 1, 1, 0]
+#
+#
+# def segregate0and1(arr):
+#     n = len(arr)
+#     count = 0
+#     for i in range(n):
+#         if arr[i] == 0:
+#             count += 1
+#
+#     for i in range(count):
+#         arr[i] = 0
+#
+#     for i in range(count,n):
+#         arr[i] = 1
+#
+#     return arr
+#
+#
+# print(segregate0and1(arr))
+
+num = 11
 
 
-def segregate0and1(arr):
-    n = len(arr)
-    count = 0
-    for i in range(n):
-        if arr[i] == 0:
-            count += 1
-
-    for i in range(count):
-        arr[i] = 0
-
-    for i in range(count,n):
-        arr[i] = 1
-
-    return arr
-
-
-print(segregate0and1(arr))
 
 
 
