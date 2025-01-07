7a07b933500d442aad8f393319491042bd6fbd5b
Vedant Pant <vedantant@gmail.com>
2025-01-08 00:34:35 +0530

    classes and objects day 2...

--- Day2/classes_and_objects.py
+++ Day2/classes_and_objects.py
@@ -1,9 +1,45 @@
-class Animal:
+class Dog:
 
-    def __init__(self, name, age):
+    def __init__(self, name):
         self.name = name
-        self.age = age
+
+    def display_name(self):
+        print(f"Dog name: {self.name}")
 
 
-dog = Animal('Dog', 5)
-print(dog.name)
+class Labrador(Dog):
+
+    def sound(self):
+        print("Labrador sound")
+
+
+class GuideDog(Labrador):
+    def guide(self):
+        print(f"{self.name}guides the way")
+
+
+class Friendly:
+
+    def greet(self):
+        print("Friendly dog")
+
+
+class GolderRetriever(Dog,Friendly):
+
+    def sound(self):
+        print("Golder Retriever sound")
+
+
+lab = Labrador("Buddy")
+lab.display_name()
+lab.sound()
+
+guide_dog = GuideDog("Tommy")
+guide_dog.display_name()
+guide_dog.sound()
+guide_dog.guide()
+
+retriever = GolderRetriever("Max")
+retriever.display_name()
+retriever.sound()
+retriever.greet()
