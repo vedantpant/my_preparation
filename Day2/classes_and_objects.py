c567cfc88267c3c449546780c9bb5dc3931e7ab8
Vedant Pant <vedantant@gmail.com>
2025-01-08 01:06:32 +0530

    classes and objects day 2...

--- Day2/classes_and_objects.py
+++ Day2/classes_and_objects.py
@@ -1,10 +1,53 @@
+# class Dog:
+#
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def display_name(self):
+#         print(f"Dog name: {self.name}")
+#
+#
+# class Labrador(Dog):
+#
+#     def sound(self):
+#         print("Labrador sound")
+#
+#
+# class GuideDog(Labrador):
+#     def guide(self):
+#         print(f"{self.name}guides the way")
+#
+#
+# class Friendly:
+#
+#     def greet(self):
+#         print("Friendly dog")
+#
+#
+# class GolderRetriever(Dog,Friendly):
+#
+#     def sound(self):
+#         print("Golder Retriever sound")
+#
+#
+# lab = Labrador("Buddy")
+# lab.display_name()
+# lab.sound()
+#
+# guide_dog = GuideDog("Tommy")
+# guide_dog.display_name()
+# guide_dog.sound()
+# guide_dog.guide()
+#
+# retriever = GolderRetriever("Max")
+# retriever.display_name()
+# retriever.sound()
+# retriever.greet()
+
 class Dog:
 
-    def __init__(self, name):
-        self.name = name
-
-    def display_name(self):
-        print(f"Dog name: {self.name}")
+    def sound(self):
+        print("dog sound")
 
 
 class Labrador(Dog):
@@ -13,33 +56,32 @@ class Labrador(Dog):
         print("Labrador sound")
 
 
-class GuideDog(Labrador):
-    def guide(self):
-        print(f"{self.name}guides the way")
-
-
-class Friendly:
-
-    def greet(self):
-        print("Friendly dog")
-
-
-class GolderRetriever(Dog,Friendly):
-
+class Beagle(Dog):
     def sound(self):
-        print("Golder Retriever sound")
+        print("Beagle sound")
 
 
-lab = Labrador("Buddy")
-lab.display_name()
-lab.sound()
+dogs = [Dog(), Labrador(), Beagle()]
 
-guide_dog = GuideDog("Tommy")
-guide_dog.display_name()
-guide_dog.sound()
-guide_dog.guide()
+for dog in dogs:
+    dog.sound()
 
-retriever = GolderRetriever("Max")
-retriever.display_name()
-retriever.sound()
-retriever.greet()
+class Calculator:
+
+    def add(self,a,b):
+        return a+b
+
+    def sub(self,a,b):
+        return a-b
+
+    def mul(self,a,b):
+        return a*b
+
+    def div(self,a,b):
+        return a/b
+
+calc = Calculator()
+print(calc.add(1,2))
+
+
+
