bd779914e1d25be1e2bc188c9ef747696873e0dd
Vedant Pant <vedantant@gmail.com>
2025-01-13 15:25:23 +0530

    continue to Day3....

--- Day4/practice_day4.py
+++ Day4/practice_day4.py
@@ -1,1 +1,19 @@
-print("Hello World")
+# find the first non-repeating character in a string
+
+str1 = "bhavay"
+
+
+def find_first_non_repeating(str1):
+    count = {}
+    for char in str1:
+        if char in count:
+            count[char] += 1
+        else:
+            count[char] = 1
+
+    for char in count:
+        if count[char] == 1:
+            return char
+
+
+print(find_first_non_repeating(str1))
