10841b260734cbab157912be0c282762beb91e31
Vedant Pant <vedantant@gmail.com>
2025-02-02 12:24:26 +0530

    commit for Day7.

---
+++ Day7/maximum_subarray.py
@@ -0,0 +1,14 @@
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+def max_subarray(nums):
+    max_sub = nums[0]
+    current_sum = 0
+
+    for n in nums:
+        if current_sum < 0:
+            current_sum = 0
+        current_sum += n
+        max_sub = max(max_sub, current_sum)
+    return max_sub
+
+
